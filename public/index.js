0599e28ddd202918c51b8da428d134350d3ab486
adrile15 <adrile1501@gmail.com>
2024-02-23 11:04:41 +0100

    gcloud

---
+++ public/index.js
@@ -0,0 +1,15 @@
+let cool = require("cool-ascii-faces");
+let express = require("express");
+
+let app = express();
+const PORT = (process.env.PORT || 20000);
+
+app.use("/", express.static("./public"));
+
+app.get("/cool", (req, res)=>{
+    res.send(`<html><body><h1>${cool()}</h1></body></html>`);
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`)
+});
