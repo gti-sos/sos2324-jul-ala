eae02cd3c4be6739afd6ccf4173d6b36c86e7e02
ahmfra <ahmfra@alum.us.es>
2024-03-31 23:10:51 +0200

    tareas ahmed

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,3 +1,203 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-cambio
+<style>
+    @import "/styles.css";
+    @import url('https://fonts.googleapis.com/css2?family=Rubik:ital,wght@0,300..900;1,300..900&display=swap');
+</style>
+
+<svelte:head>
+    <title>SOS2324-17 Project</title>
+</svelte:head>
+<script>
+    import { Container, Row, Col, Card, CardHeader, CardBody, CardTitle, CardSubtitle, CardText, Button} from '@sveltestrap/sveltestrap';
+</script>
+
+<div>
+<Container class="content-container" style="justify-content: center;"> 
+        <h1>SOS2324-17</h1>
+        <img src="/logo-sos.png" alt="SOS" class="centered-image">
+        <hr>
+        <Row>
+            <Col sm="12" md="6">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Team</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <CardSubtitle>
+                            <ul>
+                                <li><a href="https://github.com/ahmfra">Ahmed Amine Fraj</a></li>
+                                <li><a href="https://github.com/adrile15">Adrian Leon Arjona</a></li>
+                                <li><a href="https://github.com/antrummor">Antonio Rumi Moreno</a></li>
+                            </ul>
+                        </CardSubtitle>
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col sm="12" md="6">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Project description</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <CardText>Our work consists on comparing the GDB (Gross Domestic Product) of each country with the economical freedom of its citizens.</CardText>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col><br></Col>
+        </Row>
+        <Row>
+            <Col sm="12" md="6">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Repository</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <CardText><a href="https://github.com/gti-sos/SOS2324-17">gti-sos/SOS2324-17</a></CardText>
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col sm="12" md="6">
+                <Card>
+                    <CardHeader>
+                        <CardTitle>URL</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <CardText><a href="https://sos2324-17.appspot.com">sos2324-17.appspot.com</a></CardText>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col><br></Col>
+        </Row>
+        <Row>
+            <Col>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>APIs</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <div class="api-container">
+                            <Row>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="primary" href="https://sos2324-17.appspot.com/api/v1/economicfreedom_stats/">economicfreedom_stats [v1]</Button>
+                                            <Button color="primary" href="https://sos2324-17.appspot.com/api/v1/economicfreedom_stats/">economicfreedom_stats [v2]</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/ahmfra">Ahmed Amine Fraj</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="primary" href="https://sos2324-17.appspot.com/api/v1/trimestralpib_stats/" >trimestralpib_stats [v1]</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/adrile15">Adrian Leon Arjona</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="primary" href="https://sos2324-17.appspot.com/api/v1/manofthematch/">manofthematch [v1]</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/antrummor">Antonio Rumi Moreno</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </div>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>APIs documentation</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <div class="api-container">
+                            <Row>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="info" href="https://sos2324-17.appspot.com/api/v1/economicfreedom_stats/docs">economicfreedom-stats [v1]</Button>
+                                            <Button color="info" href="https://sos2324-17.appspot.com/api/v1/economicfreedom_stats/docs">economicfreedom-stats [v2]</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/ahmfra">Ahmed Amine Fraj</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="info" href="https://sos2324-17.appspot.com/api/v1/trimestralpib_stats/docs" >trimestralpib_stats [v1]</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/adrile15">Adrian Leon Arjona</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="info" href="https://sos2324-17.appspot.com/api/v1/manofthematch/docs">manofthematch [v1]</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/antrummor">Antonio Rumi Moreno</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </div>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Frontends</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+                        <div class="api-container">
+                            <Row>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="warning" href="https://sos2324-17.appspot.com/api/v1/economicfreedom_stats/">economicfreedom-stats</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/ahmfra">Ahmed Amine Fraj</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="warning" href="https://sos2324-17.appspot.com/api/v1/trimestralpib_stats/" >trimestralpib_stats</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/adrile15">Adrian Leon Arjona</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                                <Col sm="12" md="4">
+                                    <Card class="api-wrapper">
+                                        <CardBody>
+                                            <Button color="warning" href="https://sos2324-17.appspot.com/api/v1/manofthematch/">manofthematch</Button>
+                                            <CardText><hr></CardText>
+                                            <CardSubtitle>Developed by <a href="https://github.com/antrummor">Antonio Rumi Moreno</a></CardSubtitle>
+                                        </CardBody>
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </div>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+</Container>
+</div>
---
+++ front/src/routes/Header.svelte
@@ -0,0 +1,73 @@
+<script>
+    import { dev } from '$app/environment';
+    import {
+    Nav,
+    NavItem,
+    Dropdown,
+    DropdownItem,
+    DropdownToggle,
+    DropdownMenu,
+    NavLink
+  } from '@sveltestrap/sveltestrap';
+
+  let API = '/api/v1';
+    if (dev)
+        API = 'http://localhost:20000'+API;
+
+  let API_v2 = '/api/v2';
+    if (dev)
+        API_v2 = 'http://localhost:20000'+API_v2;
+
+</script>
+<style>
+    @import "/styles.css";
+</style>
+
+<main> 
+    <Nav tabs class="fixed-navbar" style="background-color: white; justify-content: center;"> 
+        <NavItem class="nav-item"> 
+            <NavLink href="/" class="nav-link d-flex align-items-center"> 
+                <img src="/home-icon.png" alt="Casa" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                Inicio 
+            </NavLink> 
+        </NavItem>
+        <Dropdown nav autoClose={true}>
+            <DropdownToggle nav class="nav-link" caret>
+                <img src="/icono-api.png" alt="API" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                APIS
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem class="menu-item" href = "{API}/economicfreedom_stats">economicfreedom_stats [v1] (Ahmed Amine Fraj)</DropdownItem>
+                <DropdownItem class="menu-item" href = "{API_v2}/economicfreedom_stats">economicfreedom_stats [v2] (Ahmed Amine Fraj)</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+        <Dropdown nav autoClose={true}>
+            <DropdownToggle nav class="nav-link" caret>
+                <img src="/icono-github.png" alt="GitHub" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                GitHub
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem class="menu-item" href = "https://github.com/ahmfra">ahmfra</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+        <Dropdown nav autoClose={true}>
+            <DropdownToggle nav class="nav-link" caret>
+                <img src="/icono-frontend.png" alt="Frontends" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                Frontends
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem class="menu-item" href = "/economicfreedom_stats">economicfreedom_stats (Ahmed Amine Fraj)</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+        <Dropdown nav autoClose={true}>
+            <DropdownToggle nav class="nav-link" caret>
+                <img src="/icono-documentacion.png" alt="Documentaciones" style="width: 20px; height: 20px; margin-right: 5px;"> 
+                Documentaciones
+            </DropdownToggle>
+            <DropdownMenu>
+                <DropdownItem class="menu-item" href = "{API}/economicfreedom_stats/docs">economicfreedom_stats [v1] (Ahmed)</DropdownItem>
+                <DropdownItem class="menu-item" href = "{API_v2}/economicfreedom_stats/docs">economicfreedom_stats [v2] (Ahmed)</DropdownItem>
+            </DropdownMenu>
+        </Dropdown>
+    </Nav>
+</main>
