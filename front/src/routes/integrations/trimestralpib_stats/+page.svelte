926c57432c484df1938dc2e56a7cd897455f6f41
adrile15 <adrile1501@gmail.com>
2024-06-27 14:11:49 +0200

    listo

--- front/src/routes/integrations/trimestralpib_stats/+page.svelte
+++ front/src/routes/integrations/trimestralpib_stats/+page.svelte
@@ -22,9 +22,9 @@
 <h1 style="text-align: center;">Redireccionar a Uso/Integracion</h1>
 <div class="button-group-container">
     <ButtonGroup>
-        <Button variant="primary" on:click={() => redireccionar('https://sos2324-17.appspot.com/integrations/trimestralpib_stats/cryptos')}>cryptos</Button>
-        <Button variant="primary" on:click={() => redireccionar('https://sos2324-17.appspot.com/integrations/trimestralpib_stats/perros')}>perros</Button>
-        <Button variant="primary" on:click={() => redireccionar('https://sos2324-17.appspot.com/integrations/trimestralpib_stats/crp')}>conProxy</Button>
-        <Button variant="primary" on:click={() => redireccionar('https://sos2324-17.appspot.com/integrations/trimestralpib_stats/leaderboard')}>Inflation vs Pib</Button>
+        <Button variant="primary" on:click={() => redireccionar('https://sos2324-jul-ala.ew.r.appspot.com/integrations/trimestralpib_stats/cryptos')}>cryptos</Button>
+        <Button variant="primary" on:click={() => redireccionar('https://sos2324-jul-ala.ew.r.appspot.com/integrations/trimestralpib_stats/perros')}>perros</Button>
+        <Button variant="primary" on:click={() => redireccionar('https://sos2324-jul-ala.ew.r.appspot.com/integrations/trimestralpib_stats/crp')}>conProxy</Button>
+        <Button variant="primary" on:click={() => redireccionar('https://sos2324-jul-ala.ew.r.appspot.com/integrations/trimestralpib_stats/leaderboard')}>Inflation vs Pib</Button>
     </ButtonGroup>
 </div>
